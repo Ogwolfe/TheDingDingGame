125efc0d1c0764ce4a1195cd8ac7b53afabbcd69
Ogwolfe <125140029+Ogwolfe@users.noreply.github.com>
2024-07-16 22:54:25 +0000

    Updated files

--- static/script.js
+++ static/script.js
@@ -1,10 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const enterButton = document.getElementById('enter-button');
+    const blurOverlay = document.getElementById('blur-overlay');
+    const content = document.getElementById('content');
 
-document.getElementById('enter-button').addEventListener('click', function() {
-    document.getElementById('blur-overlay').classList.add('hidden');
-    document.body.style.overflow = 'auto'; // Enable scrolling on the body
-    document.documentElement.style.overflow = 'auto'; // Enable scrolling on the html
-
-    console.log('Overflow for body:', document.body.style.overflow);
+    enterButton.addEventListener('click', () => {
+        blurOverlay.classList.add('hidden');
+        content.classList.add('visible');
+        document.body.style.overflow = 'auto'; // Enable scrolling
+    });
 });
 
 
@@ -18,23 +21,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const dingFolders = [
         [
-            'audio/Ding 0/Ding0thehumanity.mp3', 'audio/Ding 0/Ding0thisisatoughthing.mp3', 'audio/Ding 0/Ding0youcantdothat.mp3',
-            'Ding0youcanthandlethetruth', 'Ding0yourenogood'
+            'audio/Ding 0/Ding0thehumanity.mp3', 'audio/Ding 0/Ding0thisisatoughthing.mp3', 'audio/Ding 0/Ding0youcantdothat.mp3'
         ],
         [
-            'audio/Ding 1/Ding1babysurprise.mp3', 'audio/Ding 1/Ding1barbieworld.mp3', 'audio/Ding 1/Ding1beautiful.mp3',
-            'Ding1dontworrythatitsnotgood', 'Ding1dontyouworry', 'Ding1ihopeyourehavingfun', 'Ding1itssoeasyacaveman', 'Ding1marvellous',
-            'Ding1supernaturaldelight', 'Ding1whwnyougitajobtodo'
+            'audio/Ding 1/Ding1babysurprise.mp3', 'audio/Ding 1/Ding1barbieworld.mp3', 'audio/Ding 1/Ding1beautiful.mp3'
         ],
         [
-            'audio/Ding 2/Ding2aintyoucool.mp3', 'audio/Ding 2/Ding2cantgetenough.mp3', 'audio/Ding 2/Ding2celebrate.mp3',
-            'Ding2feelssogood', 'Ding2greatgodinheaven', 'Ding2hatisoff', 'Ding2iloveyou', 'Ding2ineverseensuch', 'Ding2itsmagic',
-            'Ding2thewonderofitall'
+            'audio/Ding 2/Ding2aintyoucool.mp3', 'audio/Ding 2/Ding2cantgetenough.mp3', 'audio/Ding 2/Ding2celebrate.mp3'
         ],
         [
-            'audio/Ding 3/Ding3itsamiracle.mp3', 'audio/Ding 3/Ding3loveyahoney.mp3', 'audio/Ding 3/Ding3nobodydoesitbetter.mp3',
-            'Ding3skyrockets', 'Ding3sogood', 'Ding3thatsawhatilike', 'Ding3thatsthewsyilikeit', 'Ding3theforce', 'Ding3theforce',
-            'Ding3youreasuperstar'
+            'audio/Ding 3/Ding3itsamiracle.mp3', 'audio/Ding 3/Ding3loveyahoney.mp3', 'audio/Ding 3/Ding3nobodydoesitbetter.mp3'
         ]
     ];
 
